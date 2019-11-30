48ec80abf45a2b3cad56377135a9703f9cb8d1f3
tebaly <support@freedomsex.net>
2019-11-30 03:31:01 +0300

    feat-summary

---
+++ ColoredItem.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  props: ['text', 'index', 'type', 'marker'],
+  computed: {
+    status() {
+      let values = {
+        'success': 1,
+        'info': 2,
+        'warning': 4,
+        'danger': 8,
+      };
+      return values[this.type] || 0;
+    },
+    value() {
+      return this.marker !== undefined ? this.marker : this.status;
+    },
+    colored() {
+      return {
+        success: this.value === 1,
+        info: this.value === 2,
+        warning: this.value === 4,
+        danger: this.value === 8,
+        skiped: !this.value,
+      };
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="marker-text-colored" :class="colored" @click="$emit('select', index)">
+    <slot>
+      {{text}}
+    </slot>
+  </div>
+</template>
+
+<style lang="less">
+.marker-text-colored {
+  color: @dark;
+  font-size: @font-md;
+  margin-bottom: @indent-xs;
+  padding: @indent-sm;
+  border-style: solid;
+  border-width: 0 0 0 @border-xl;
+  background: @light;
+  position: relative;
+
+  &.success {
+    border-color: @green;
+  }
+  &.info {
+    border-color: @violet;
+  }
+  &.warning {
+    border-color: @orange-light;
+  }
+  &.danger {
+    border-color: @red;
+  }
+  &.skiped {
+    border-color: @gray;
+  }
+}
+</style>
--- index.vue
+++ index.vue
@@ -1,65 +1,31 @@
 <script>
+import ColoredItem from './ColoredItem.vue'
+
 export default {
   props: ['text', 'index', 'type', 'marker'],
-  computed: {
-    status() {
-      let values = {
-        'success': 1,
-        'info': 2,
-        'warning': 4,
-        'danger': 8,
-      };
-      return values[this.type] || 0;
-    },
-    value() {
-      return this.marker !== undefined ? this.marker : this.status;
-    },
-    colored() {
-      return {
-        success: this.value === 1,
-        info: this.value === 2,
-        warning: this.value === 4,
-        danger: this.value === 8,
-        skiped: !this.value,
-      };
-    },
+  components: {
+    ColoredItem,
   },
 };
 </script>
 
 <template>
-  <div class="marker-text-colored" :class="colored" @click="$emit('select', index)">
-    <slot>
-      {{text}}
-    </slot>
+<div>
+  <div class="marker-text-colored__summary">
+    <slot name="summary"></slot>
   </div>
+  <ColoredItem :type="type" :marker="marker" @select="$emit('select', index)">
+    {{text}}
+  </ColoredItem>  
+</div> 
 </template>
 
 <style lang="less">
 .marker-text-colored {
-  color: @dark;
-  font-size: @font-md;
-  margin-bottom: @indent-xs;
-  padding: @indent-sm;
-  border-style: solid;
-  border-width: 0 0 0 @border-xl;
-  background: @light;
-  position: relative;
-
-  &.success {
-    border-color: @green;
-  }
-  &.info {
-    border-color: @violet;
-  }
-  &.warning {
-    border-color: @orange-light;
-  }
-  &.danger {
-    border-color: @red;
-  }
-  &.skiped {
-    border-color: @gray;
+  &__summary {
+    font-size: @font-sm; 
+    padding-bottom: @indent-xs; 
+    color: @gray-dark; 
   }
 }
 </style>
